ee0844af86eb2908fce4e48ad1d7c5743e69ba9a
szdytom <szdytom@163.com>
2020-10-23 22:13:54 +0800

    Add setup file to set up oh-my-emacs

--- functions/init.el
+++ functions/init.el
@@ -1,1 +1,2 @@
 (load-config-file "functions/reload-config.el")
+(load-config-file "functions/setup.el")
---
+++ functions/setup.el
@@ -0,0 +1,23 @@
+(defun ohmyemacs-install-list-packages
+  (pl)
+  (loop for x in pl do
+        (print x)(
+         package-install x)))
+
+(setf ohmyemacs-required-packages
+      '(neotree
+     flycheck
+     lsp-mode
+     lsp-ui
+     doom-themes
+     mood-line
+     company-lsp
+     python-mode
+     xclip
+     s
+     use-package))
+
+(defun ohmyemacs-setup
+    ()
+  (interactive)
+  (ohmyemacs-install-list-packages ohmyemacs-required-packages))
